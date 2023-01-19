3fe63185e3a5c8109a4cb32aaac513359eacd1b0
Kirk Lin <linkirk@163.com>
2023-01-19 15:08:10 +0800

    feat: init

---
+++ scripts/copy.js
@@ -0,0 +1,25 @@
+import fs from "fs";
+
+fs.copyFileSync(
+    "node_modules/@csstools/normalize.css/normalize.css",
+    "normalize.css",
+);
+
+if (!fs.existsSync("sanitize")) {
+    fs.mkdirSync("sanitize");
+}
+
+for (const stylesheet of [
+    "sanitize.css",
+    "forms.css",
+    "assets.css",
+    "typography.css",
+    "reduce-motion.css",
+    "system-ui.css",
+    "ui-monospace.css",
+]) {
+    fs.copyFileSync(
+        `node_modules/sanitize.css/${stylesheet}`,
+        `sanitize/${stylesheet}`,
+    );
+}
